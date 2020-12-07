d5074e36ea526a59fafed50d0cf156c64dee550f
Justin Auger <justinauger@gmail.com>
2020-12-07 09:11:21 +0000

    day 7 part 2

--- 07/07.cpp
+++ 07/07.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 set<string> holds_shinygold;
 map<string, vector<string>> direct_parent;
+map<string, vector<pair<string,int>>> child;
 
 void f(string x) {
     if (holds_shinygold.count(x) != 0)
@@ -17,6 +18,23 @@ void f(string x) {
             f(y);
         }
     }
+}
+
+map<string, int> dfs_state;
+vector<string> exit_order;
+
+void dfs(string x) {
+    if(dfs_state[x] != 0) return;
+
+    cout << "ENTER " << x << endl;
+    dfs_state[x] = 1;
+
+    for(auto y : child[x])
+        dfs(y.first);
+
+    cout << "EXIT " << x << endl;
+    exit_order.push_back(x);
+    dfs_state[x] = 2;
 }
 
 int main() {
@@ -35,16 +53,17 @@ int main() {
         getline(cin,contents);
 
         cout << m1 << m2 << ":" << endl; // << contents << endl;
-        vector<string> v;
-        if(contents != " no other bags."){
+        if(contents != " no other bags.") {
             std::smatch res;
             std::regex regex("(\\d*) (\\w*) (\\w*) bags?");
 
             string::const_iterator searchStart( contents.cbegin() );
             while(std::regex_search(searchStart, contents.cend(), res, regex)) {
                 string desc = res.str(2) + res.str(3);
-                cout << desc << " " << res[1] << endl;
+                string count = res.str(1);
+                cout << desc << " " << count << endl;
                 direct_parent[desc].push_back(m1 + m2);
+                child[m1+m2].push_back({desc,stoi(count)});
 
                 searchStart = res.suffix().first;
             }
@@ -68,6 +87,17 @@ int main() {
         f(x);
     }
     cout << holds_shinygold.size() << endl;
+
+    dfs("shinygold");
+
+    map<string, int> num_bags_inside;
+
+    for(auto s : exit_order) {
+        for(auto y:child[s]) {
+            num_bags_inside[s] += y.second*(num_bags_inside[y.first] + 1);
+        }
+    }
+    cout << num_bags_inside["shinygold"] << endl;
 
     return 0;
 }
