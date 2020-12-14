0e265eff7f8a034981272eea6211389cd8f4c8cf
Justin Auger <justinauger@gmail.com>
2020-12-14 08:56:51 +0000

    d14 p2

--- 14/14.cpp
+++ 14/14.cpp
@@ -13,13 +13,27 @@ int64_t build_mask(string s, char t) {
     return mask;
 }
 
+map<int64_t, int64_t> mem;
+
+void f(int64_t index, int64_t mask, int64_t val, int i) {
+    if(i == 36) {
+        mem[index]=val; return;
+    }
+    if(mask&(1ll<<i)){
+        f(index|(1ll<<i), mask, val, i+1);
+        f(index&(~(1ll<<i)), mask, val, i+1);
+        return;
+    }
+    f(index, mask, val, i+1);
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    map<int, int64_t> mem;
     int64_t mask0=0;
     int64_t mask1=0;
+    int64_t maskX=0;
 
     // IO
     regex mask_regex("mask = ([X01]{36})");
@@ -32,16 +46,21 @@ int main() {
             cout << "mask " << res[1] << endl;
             mask0 = build_mask(res[1].str(), '0');
             mask1 = build_mask(res[1].str(), '1');
+            maskX = build_mask(res[1].str(), 'X');
         } else if(regex_match(line, res, mem_regex)) {
             cout << "memset " << res[1] << " " << res[2] << endl;
-            int index = stoi(res[1].str());
+            int64_t index = stoi(res[1].str());
             int64_t x = stoi(res[2].str());
 
-            // apply masks
-            x |= mask1;
-            x &= ~mask0;
-            cout << "setting " << index << " to " << x << endl;
-            mem[index] = x;
+            // apply masks - part1
+            // x |= mask1;
+            // x &= ~mask0;
+            // cout << "setting " << index << " to " << x << endl;
+            // mem[index] = x;
+
+            index|=mask1;
+
+            f(index, maskX, x, 0);
         } else {
             cout << "Couldn't parse line" << endl;
             exit(-1);
@@ -49,8 +68,10 @@ int main() {
     }
 
     int64_t sum = 0;
-    for(auto [k,v]:mem)
+    for(auto [k,v]:mem){
+        cout << k << ":" << v << endl;
         sum += v;
+    }
     cout << "sum=" << sum << endl;
 
     return 0;
