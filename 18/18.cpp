cfc29a5d380f9b002acbe2f28bafae48aaeca0f3
Justin Auger <justinauger@gmail.com>
2020-12-18 09:29:01 +0000

    d18p2

--- 18/18.cpp
+++ 18/18.cpp
@@ -8,9 +8,9 @@ int64_t f(const string::iterator s, const string::iterator t) {
     // cout << endl;
 
     int d = 0;
-    char op = '\0';
-    int64_t ret;
     string::iterator a,b;
+    list<int64_t> x_arr;
+    list<char> op_arr;
     for(auto it = s; it != t; it++) {
         if(*it == '(') {
             d++;
@@ -23,21 +23,42 @@ int64_t f(const string::iterator s, const string::iterator t) {
             if(d == 0) {
                 b = it;
                 int64_t x = f(a,b);
-                if(op == '\0') ret = x;
-                if(op == '+')  ret += x;
-                if(op == '*')  ret *= x;
+                x_arr.push_back(x);
             }
         }
-        else if(d == 0 && *it == '+') op = '+';
-        else if(d == 0 && *it == '*') op = '*';
+        else if(d == 0 && *it == '+') op_arr.push_back('+');
+        else if(d == 0 && *it == '*') op_arr.push_back('*');
         else if(d == 0 && *it >= '0' && *it <= '9') {
             int64_t x = *it-'0';
-            if(op == '\0') ret = x;
-            if(op == '+')  ret += x;
-            if(op == '*')  ret *= x;
+            x_arr.push_back(x);
         }
     };
+
+    assert(x_arr.size() == op_arr.size() + 1);
     assert(d==0);
+    int N = x_arr.size();
+
+    // pass 1 for addition
+    auto it = next(x_arr.begin());
+    auto it1 = op_arr.begin();
+
+    while(it != x_arr.end()) {
+        if(*it1 == '+') {
+            *(prev(it)) += *it;
+            it = x_arr.erase(it);
+            it1 = op_arr.erase(it1);
+        } 
+        else {
+            it++;
+            it1++;
+        }
+    }
+    assert(it1 == op_arr.end());
+
+    int64_t ret = 1;
+    // pass 2 for multiplication
+    for(auto x : x_arr)
+        ret *= x;
     
     for(auto it = s; it != t; it++)
         cout << *it;
