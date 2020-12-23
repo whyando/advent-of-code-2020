8432afeb876ccf130335180aa2a0edf882a11fc7
Justin Auger <justinauger@gmail.com>
2020-12-23 12:35:45 +0000

    d23p2

--- 23/23.cpp
+++ 23/23.cpp
@@ -2,38 +2,46 @@
 
 using namespace std;
 
-int decr(int x){
-    if(x == 1) return 9;
+const int N = 1000000;
+
+constexpr int decr(int x){
+    if(x == 0) return N-1;
     return x-1;
 }
 
 int main() {
     // const vector<int> input = {3,8,9,1,2,5,4,6,7};
     const vector<int> input = {7,9,2,8,4,5,1,3,6};
-    deque<int> v(input.begin(), input.end());
 
-    for(int T=0;T<100;T++) {
-        for(auto x : v)
-            cout << x << " ";
-        cout << endl;
+    vector<int> next(N);
+    iota(next.begin(), next.end(), 1);
+    next[N-1] = input[0]-1;
+    for(int i=0;i<input.size()-1;i++)
+        next[input[i]-1] = input[i+1]-1;
+    if(input.size() != N) next[input.back()-1] = input.size();
+    else                  next[input.back()-1] = input[0]-1;
 
-        int a = v.front(); v.pop_front();
-        int x = v.front(); v.pop_front();
-        int y = v.front(); v.pop_front();
-        int z = v.front(); v.pop_front();
-        int t = decr(a);
-        while(t == x || t == y || t == z)
-            t = decr(t);
-        auto k = find(v.begin(), v.end(), t);
-        k = v.insert(k + 1, x);
-        k = v.insert(k + 1, y);
-        k = v.insert(k + 1, z);
-        v.insert(v.end(), a);
+    int cur = input[0]-1;
+    for(int tt=0;tt<10000000;tt++) {
+        const int A = next[cur];
+        const int B = next[A];
+        const int C = next[B];
+        const int D = next[C];
+
+        int T = decr(cur);
+        while(T == A || T == B || T == C)
+            T = decr(T);
+        
+        next[cur] = D;
+        next[C] = next[T];
+        next[T] = A;    
+
+        cur = next[cur];
     }
 
-    for(auto x : v)
-        cout << x << " ";
-    cout << endl;
+    cout << next[0]+1 << endl;
+    cout << next[next[0]]+1 << endl;
+    cout << (int64_t)(next[0]+1) * (next[next[0]]+1) << endl;
 
     return 0;
 }
